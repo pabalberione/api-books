00f5bb057da1d9bb0a001be49788ef726ab6a6ad
Pabalberione <pabalberione@gmail.com>
2024-06-13 06:17:26 -0300

    First commit

---
+++ src/test/java/com/api_books/ApiBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api_books;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
